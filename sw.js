8410960c06a4cb0a7716a93a363542e7ffd3d0da
11YearGoat <aditya.vare9@gmail.com>
2025-08-02 16:16:58 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("static").then(cache => {
+      return cache.addAll(["index.html", "icon-512.png"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
